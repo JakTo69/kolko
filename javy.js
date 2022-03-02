295c4a63da03808ae7d0b821981d181de5c19bf0
JakTo69 <74741088+JakTo69@users.noreply.github.com>
2022-03-02 22:24:22 +0100

    Update javy.js

--- javy.js
+++ javy.js
@@ -1,96 +1,47 @@
-const plansza = document.getElementById("plansza");
-const boxes = Array.from(document.getElementsByClassName("box"));
-const restart1 = document.getElementById("restart1");
-const txt1 = document.getElementById("txt1");
-const pole = [null, null, null, null, null, null, null, null, null];
-const O_TEXT = "O";
-const X_TEXT = "X";
-let Gracz1 = O_TEXT;
+const Gracz1 = 'fa-circle-o';
+const Gracz2 = 'fa-times';
+let runda = 1;
+const plansza = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+const kombinacje = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
 
-const utowrzplansze = () => {
-  boxes.forEach((box, index) => {
-    let styleString = "";
-    if (index < 3) {
-      styleString += `border-bottom: 3px solid var(--RED);`;
-    }
-    if (index % 3 === 0) {
-      styleString += `border-right: 3px solid var(--RED);`;
-    }
-    if (index % 3 === 2) {
-      styleString += `border-left: 3px solid var(--RED);`;
-    }
-    if (index > 5) {
-      styleString += `border-top: 3px solid var(--RED);`;
-    }
-    box.style = styleString;
+const boxes = [...document.querySelectorAll('.box')];
+boxes.forEach(box => box.addEventListener('click', wybór));
 
-    box.addEventListener("click", boxklik);
-  });
-};
+function wybierz(event) {
+    const { x, y } = event.target.dataset;
+    const tura = runda % 2 === 0 ? Gracz2 : Gracz1;
+    if (plansza[x][y] !== '') return;
+    event.target.classList.add(tura);
+    plansza[x][y] = tura;
+    runda++;
 
-function boxklik(e) {
-  const id = e.target.id;
-  if (!pole[id]) {
-    pole[id] = Gracz1;
-    e.target.innerText = Gracz1;
-    if (zwyciestwo(Gracz1)) {
-      playText.innerHTML = `${Gracz1} wins!!`;
-      return;
-    }
-    Gracz1 = Gracz1 === O_TEXT ? X_TEXT : O_TEXT;
-  }
+    console.log(check());
 }
 
-const zwyciestwo = (gracz) => {
+function check() {
+    const wynik = plansza.reduce((total, x) => total.concat(x));
+    let zwyciezca = null;
+    let ruch = {
+        'fa-times': [],
+        'fa-circle-o': []
+    };
+    wynik.forEach((pole, index) => ruch[pole] ? ruch[pole].push(index) : null);
+    combinations.forEach(combination => {
+        if (kombinacje.every(index => ruch[Gracz1].indexOf(index) > -1)) {
+          zwyciezca = 'Gracz X wygrał';
+        }
+        if (kombinacje.every(index => ruch[Gracz2].indexOf(index) > -1)) {
+          zwyciezca = 'Gracz O wygrał';
+        }
+    });
 
-  if (pole[0] === gracz) {
-    if (pole[1] === gracz && pole[2] === gracz) {
-      console.log(`${gracz} wins up top`);
-      return true;
-    }
-    if (pole[3] === gracz && pole[6] === gracz) {
-      console.log(`${gracz} wins on the left`);
-      return true;
-    }
-    if (pole[4] === gracz && pole[8] === gracz) {
-      console.log(`${gracz} wins on the diagonal`);
-      return true;
-    }
-  }
-
-  if (pole[8] === gracz) {
-    if (pole[2] === gracz && pole[5] === gracz) {
-      console.log(`${gracz} wins on the right`);
-      return true;
-    }
-    if (pole[7] === gracz && pole[6] === gracz) {
-      console.log(`${gracz} wins on the bottom`);
-      return true;
-    }
-  }
-
-  if (pole[4] === gracz) {
-    if (pole[3] === gracz && pole[5] === gracz) {
-      console.log(`${gracz} wins on the middle horizontal`);
-      return true;
-    }
-    if (pole[1] === gracz && pole[7] === gracz) {
-      console.log(`${gracz} wins on the middle vertical`);
-      return true;
-    }
-  }
-};
-
-restart1.addEventListener("click", () => {
-  pole.forEach((space, index) => {
-    pole[index] = null;
-  });
-  boxes.forEach((box) => {
-    box.innerText = "";
-  });
-  txt1.innerHTML = `Let's Play!!`;
-
-  Gracz1 = O_TEXT;
-});
-
-drawBoard();
+    return zwyciezca;
+}
